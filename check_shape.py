88c780b90bc14a77dddb074c638941da48d8a8b9
RaneshChandraKatta <katta.ranesh.chandra@gmail.com>
2026-03-24 10:09:32 +0530

    Remove unnecessary debug scripts

--- check_shape.py
+++
@@ -1,7 +0,0 @@
-from keras import models
-
-model = models.load_model('static/model/bird_species.h5')
-print("Model Inputs:", model.inputs)
-print("Input Shape:", model.input_shape)
-for layer in model.layers:
-    print(layer.name, layer.input_shape, layer.output_shape)
--- inspect_model.py
+++
@@ -1,4 +0,0 @@
-from keras import models
-
-model = models.load_model('static/model/bird_species.h5')
-print(model.summary())
